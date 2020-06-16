59ef46d62f2883925397372a272c34fa5d4fb5d5
Lily  Pagan <lilypagan@Lilys-MacBook-Pro.local>
2020-06-15 23:26:05 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Xenophobia/Frontend-React/precache-manifest.b112a36754eafa1c7e5d74440da6a0c3.js"
+  "/Xenophobia/Frontend-React.git/precache-manifest.ebc4437527a9e22912938a7774ded030.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Xenophobia/Frontend-React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Xenophobia/Frontend-React.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
