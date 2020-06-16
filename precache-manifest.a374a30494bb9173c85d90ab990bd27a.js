07d262e437e759a65a901bdd8a83bd0ebb301b27
Lily  Pagan <lilypagan@Lilys-MacBook-Pro.local>
2020-06-16 11:18:28 -0400

    Updates

---
+++ precache-manifest.a374a30494bb9173c85d90ab990bd27a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946bd75b532a2f60e8c7d98c31b1bcc5",
+    "url": "/Xenophobia-Meter/index.html"
+  },
+  {
+    "revision": "86825b0f306a2e8ef8c0",
+    "url": "/Xenophobia-Meter/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "30377f2f9e41c3e90454",
+    "url": "/Xenophobia-Meter/static/css/main.64f2fca7.chunk.css"
+  },
+  {
+    "revision": "86825b0f306a2e8ef8c0",
+    "url": "/Xenophobia-Meter/static/js/2.9bf4769f.chunk.js"
+  },
+  {
+    "revision": "e5b5915937dd17bb1d57c8e9630f5703",
+    "url": "/Xenophobia-Meter/static/js/2.9bf4769f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30377f2f9e41c3e90454",
+    "url": "/Xenophobia-Meter/static/js/main.19f9c585.chunk.js"
+  },
+  {
+    "revision": "708a72a2ab2698e78c40",
+    "url": "/Xenophobia-Meter/static/js/runtime-main.e7f2b0ef.js"
+  }
+]);
